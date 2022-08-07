dfb8205a2f5919f26226053effa101a009fc1e74
Rich Coutts <buttonsrtoys@gmail.com>
2022-08-07 05:37:22 -0700

    Add View.get, View.listenTo, and ViewWithStatelessViewModel (#13)
    added ViewWithStatelessViewModel, View.get, and View.listenTo

--- lib/mvvm_plus.dart
+++ lib/mvvm_plus.dart
@@ -14,11 +14,7 @@ import 'package:registrar/registrar.dart';
 ///     }
 ///   }
 ///
-/// As shown, you specify a generic and a builder for your ViewModel subclass.
-///
 /// [viewModelBuilder] is a builder for a [ViewModel] subclass.
-///
-/// See the [README](https://github.com/buttonsrtoys/mvvm_plus/blob/main/README.md) for more information.
 abstract class View<T extends ViewModel> extends StatefulWidget {
   View({
     required this.viewModelBuilder,
@@ -27,41 +23,59 @@ abstract class View<T extends ViewModel> extends StatefulWidget {
   final T Function() viewModelBuilder;
   final _stateInstance = _StateInstance<T>();
 
-  /// Returns the [ViewModel] subclass associated with this [View].
+  /// Returns the [ViewModel] subclass bound to this [View].
   T get viewModel => _stateInstance.value._viewModel;
 
-  /// See [State.context] for details.
+  /// Get a registered object.
+  ///
+  /// Uses [ViewModel.get] to get a registered object. (See [ViewModel.get] for more details.)
+  @protected
+  U get<U extends Object>({String? name}) => viewModel.get<U>(name: name);
+
+  /// Get a registered ChangeNotifier and adds a [ViewModel.buildView] listener to [U].
+  ///
+  /// On calls to [U.notifyListeners], queues [build]. Note that unlike [ViewModel.listenTo], [View.listenTo] has
+  /// no "listener" parameter that receives a custom listener. If you need a custom listener, please define it in your
+  /// custom [ViewModel] and use [ViewModel.listenTo] so that your business logic is relegated to your [ViewModel].
+  /// (See [ViewModel.listenTo] for more details.)
+  ///
+  /// [name] is the optional name assigned to the ChangeNotifier when it was registered.
+  @protected
+  U listenTo<U extends ChangeNotifier>({String? name}) => viewModel.listenTo<U>(name: name);
+
+  /// Same functionality as [State.context].
   BuildContext get context => _stateInstance.value.context;
 
-  /// See [State.mounted] for details.
+  /// Same functionality as [State.mounted].
   bool get mounted => _stateInstance.value.mounted;
 
-  /// See [State.didUpdateWidget] for details.
+  /// Same functionality as [State.didUpdateWidget].
   @protected
   @mustCallSuper
   void didUpdateWidget(covariant View<T> oldWidget) {}
 
-  /// See [State.reassemble] for details.
+  /// Same functionality as [State.reassemble].
   @protected
   @mustCallSuper
   void reassemble() {}
 
-  /// See [State.deactivate] for details.
+  /// Same functionality as [State.deactivate].
   @protected
   @mustCallSuper
   void deactivate() {}
 
-  /// See [State.activate] for details.
+  /// Same functionality as [State.activate].
   @protected
   @mustCallSuper
   void activate() {}
 
-  /// See [State.didChangeDependencies] for details.
+  /// Same functionality as [State.didChangeDependencies].
   @protected
   @mustCallSuper
   void didChangeDependencies() {}
 
   /// Same functionality as [StatelessWidget.build]. E.g., override this function to define the interface.
+  @protected
   Widget build(BuildContext context);
 
   /// [createState] provides the logic for this [View] class and typically would not be overridden. To specify the
@@ -71,6 +85,7 @@ abstract class View<T extends ViewModel> extends StatefulWidget {
   State<View<T>> createState() => _ViewState<T>();
 }
 
+/// mvvm_plus implementation.
 class _ViewState<T extends ViewModel> extends State<View<T>> {
   late T _viewModel;
 
@@ -130,12 +145,14 @@ class _ViewState<T extends ViewModel> extends State<View<T>> {
   }
 }
 
-/// Wrapper for ViewModel to transfer from [_ViewState] to [View]
+/// Wrapper for ViewModel so that [View] can access the ViewModel in [_ViewState].
 class _StateInstance<T extends ViewModel> {
   late _ViewState<T> value;
 }
 
-/// [ChangeNotifier] subscription
+/// ChangeNotifier subscription.
+///
+/// A subscription to a ChangeNotifier that is managed.
 class _Subscription extends Equatable {
   const _Subscription({required this.changeNotifier, required this.listener});
 
@@ -152,11 +169,10 @@ class _Subscription extends Equatable {
 
 /// Base class for View Models
 ///
-/// [register] is whether the built [ViewModel] is "registered", meaning that it can be located with from other widgets
-/// by using [ViewModel.get] (which uses [Registrar.get] under the hood, so you can use [Registrar.get] as well). View
-/// Models are typically only registered when they need to be located by a descendant of this
-/// widget or by a widget on another branch of the widget tree. Note that the [View] uses member [viewModel] to access
-/// its [ViewModel], so doesn't need the registry or to use [get].
+/// [register] is whether the built [ViewModel] should "register", meaning that it can be located using
+/// [ViewModel.get], [View.get], [ViewModel.listenTo], or [View.listenTo], Models are typically only registered when
+/// they need to be located by a descendant of this widget or by a widget on another branch of the widget tree. Note
+/// that the [View] uses member [viewModel] to access its [ViewModel], so doesn't need the registry or to use [get].
 /// [name] is the optional unique name of the registered View Model. Typically registered View Models are not named.
 /// On rare occasions when multiple View Models of the same type are registered, unique names uniquely identify them.
 abstract class ViewModel extends ChangeNotifier {
@@ -191,9 +207,9 @@ abstract class ViewModel extends ChangeNotifier {
   @protected
   late void Function() buildView;
 
-  /// Gets a registered object.
+  /// Get a registered object.
   ///
-  /// This method performs a one-time retrieval and does not listen to future changes. To listen for future changes,
+  /// This method gets a registered object and does not listen to future changes. To listen for future changes,
   /// see [listenTo].
   ///
   /// [name] is the optional name assigned to the object when it was registered.
@@ -203,14 +219,16 @@ abstract class ViewModel extends ChangeNotifier {
     return Registrar.get<T>(name: name);
   }
 
-  /// Gets a registered ChangeNotifier and listens to future calls to [T.notifyListeners].
+  /// Get a registered ChangeNotifier and listens to future calls to [T.notifyListeners].
   ///
-  /// [name] is the optional name assigned to the object when it was registered.
-  /// [listener] is the optional listener that is called every time [T.notifyListeners] is called. If [listener] is
-  /// null then [buildView] is added as a listener. Usages like below result in [View] being queued to rebuild every
-  /// time [SomeChangeNotifier.notifyListeners] is called:
+  /// [name] is the optional name assigned to the ChangeNotifier when it was registered.
+  /// [listener] is the optional listener that is added one time and called every time [T.notifyListeners] is called.
+  /// If [listener] is null then [buildView] is added as a listener. Usages like below result in [View] being queued to
+  /// rebuild every time [SomeChangeNotifier.notifyListeners] is called:
   ///
   ///     int get someInt => listenTo<SomeChangeNotifier>().someInt;
+  ///
+  /// or another example:
   ///
   ///     int doubleSomeInt() {
   ///       return 2 * listenTo<SomeChangeNotifier>().someInt;
@@ -223,14 +241,15 @@ abstract class ViewModel extends ChangeNotifier {
   ///       buildView();
   ///     }
   ///
-  /// Specifying a [listener] would typically be done in a constructor or initState and the returned ChangeNotifier
-  /// would typically be ignored:
+  /// Specifying a [listener] would typically be done in a constructor or initState:
   ///
-  ///    @override
-  ///    void initState() {
-  ///      super.initState();
-  ///      listenTo<SomeChangeNotifier>(listener: myListener);
-  ///    }
+  ///     late final SomeChangeNotifier someChangeNotifier;
+  ///
+  ///     @override
+  ///     void initState() {
+  ///       super.initState();
+  ///       someChangeNotifier = listenTo<SomeChangeNotifier>(listener: myListener);
+  ///     }
   ///
   /// Listeners are only added to [T] once regardless of the number of times [listenTo] is called.
   @protected
@@ -267,6 +286,46 @@ abstract class ViewModel extends ChangeNotifier {
     }
     super.dispose();
   }
+}
+
+/// Empty ViewModel used by [ViewWithStatelessViewModel]
+class _StatelessViewModel extends ViewModel {}
+
+/// A [View] with a predefined [ViewModel] that has no states.
+///
+/// This is a convenience class for creating Views that don't have any states but update on changes to registered
+/// ChangeNotifiers. E.g., a widget that listens to a service but doesn't have its own states.
+///
+/// So, if a View has no states but you want to listen to a registered ChangeNotifier, instead of creating an empty
+/// ViewModel and a View that consumes it:
+///
+///     class MyStatelessViewModel extends ViewModel {}
+///
+///     class MyView extends View<MyStatelessViewModel> {
+///       MyView({super.key}) : super(viewModelBuilder: () => MyStatelessViewModel());
+///       @override
+///       Widget build(BuildContext context) {
+///         return Text(get<SomeNotifier>().text);
+///       }
+///     }
+///
+/// you can instead write
+///
+///     class MyView extends ViewWithStatelessViewModel {
+///       MyView({super.key});
+///       @override
+///       Widget build(BuildContext context) {
+///         return Text(get<SomeNotifier>().text);
+///       }
+///     }
+///
+/// Under the hood, an empty ViewModel is created for [ViewWithStatelessViewModel]
+abstract class ViewWithStatelessViewModel extends View<_StatelessViewModel> {
+  ViewWithStatelessViewModel({super.key}) : super(viewModelBuilder: () => _StatelessViewModel());
+
+  @override
+  @protected
+  Widget build(BuildContext context);
 }
 
 String _missingGenericError(String function, String type) =>
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,6 +1,4 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:registrar/registrar.dart';
 import 'package:mvvm_plus/mvvm_plus.dart';
@@ -33,7 +31,12 @@ Widget testApp({
 
 /// The [Registrar] service
 class MyNotifier extends ChangeNotifier {
-  final number = _number;
+  int number = _number;
+
+  void incrementNumber() {
+    number++;
+    notifyListeners();
+  }
 }
 
 /// The [View]
@@ -44,11 +47,12 @@ class MyWidget extends View<MyWidgetViewModel> {
     required bool register,
     required String? name,
   }) : super(
-            viewModelBuilder: () => MyWidgetViewModel(
-                  listen: listen,
-                  register: register,
-                  name: name,
-                ));
+      viewModelBuilder: () =>
+          MyWidgetViewModel(
+            listen: listen,
+            register: register,
+            name: name,
+          ));
 
   @override
   Widget build(BuildContext _) {
@@ -78,10 +82,36 @@ class MyWidgetViewModel extends ViewModel {
       // listen twice so can later test that only one listener added
       listenTo<MyNotifier>(); // 1st listen
       myNotifier = listenTo<MyNotifier>(); // 2nd listen
+    } else {
+      myNotifier = get<MyNotifier>();
     }
   }
 
   int get number => myNotifier.number;
+}
+
+/// Test app for widget subclassed from [ViewWithStatelessViewModel]
+Widget statelessTestApp({required bool listen}) =>
+    MaterialApp(
+      home: Registrar(
+        builder: () => MyNotifier(),
+        child: MyStatelessView(listen: listen),
+      ),
+    );
+
+/// The [Registrar] service
+class MyStatelessView extends ViewWithStatelessViewModel {
+  MyStatelessView({
+    super.key,
+    required this.listen,
+  });
+
+  final bool listen;
+
+  @override
+  Widget build(BuildContext context) {
+    return listen ? Text('${listenTo<MyNotifier>().number}') : Text('${get<MyNotifier>().number}');
+  }
 }
 
 void main() {
@@ -98,28 +128,87 @@ void main() {
   });
 
   group('MyWidget', () {
-    testWidgets('not registered', (WidgetTester tester) async {
+    testWidgets('not listening, registered, or named ViewModel does not update value', (WidgetTester tester) async {
+      await tester.pumpWidget(testApp(listen: false, register: false, name: null));
+
+      expect(Registrar.isRegistered<MyNotifier>(), true);
+      expect(find.text('$_number'), findsOneWidget);
+
+      Registrar.get<MyNotifier>().incrementNumber();
+      await tester.pump();
+
+      // expect does not increment b/c not listening
+      expect(find.text('$_number'), findsOneWidget);
+    });
+
+    testWidgets('listening but not registered ViewModel shows correct values', (WidgetTester tester) async {
       await tester.pumpWidget(testApp(listen: true, register: false, name: null));
 
       expect(Registrar.isRegistered<MyNotifier>(), true);
       expect(find.text('$_number'), findsOneWidget);
+
+      Registrar.get<MyNotifier>().incrementNumber();
+      await tester.pump();
+
+      expect(find.text('${_number + 1}'), findsOneWidget);
     });
 
-    testWidgets('register, not named', (WidgetTester tester) async {
+    testWidgets('listening and registered but not named ViewModel shows correct values', (WidgetTester tester) async {
       await tester.pumpWidget(testApp(listen: true, register: true, name: null));
 
       expect(find.text('$_number'), findsOneWidget);
       expect(Registrar.isRegistered<MyWidgetViewModel>(), true);
-      expect(Registrar.get<MyWidgetViewModel>().number, _number);
+      expect(Registrar
+          .get<MyWidgetViewModel>()
+          .number, _number);
+
+      Registrar.get<MyNotifier>().incrementNumber();
+      await tester.pump();
+
+      expect(find.text('${_number + 1}'), findsOneWidget);
     });
 
-    testWidgets('register, named', (WidgetTester tester) async {
+    testWidgets('listening, registered, and named ViewModel shows correct values', (WidgetTester tester) async {
       await tester.pumpWidget(testApp(listen: true, register: true, name: _viewModelName));
 
       expect(find.text('$_number'), findsOneWidget);
       expect(Registrar.isRegistered<MyWidgetViewModel>(), false);
       expect(Registrar.isRegistered<MyWidgetViewModel>(name: _viewModelName), true);
-      expect(Registrar.get<MyWidgetViewModel>(name: _viewModelName).number, _number);
+      expect(Registrar
+          .get<MyWidgetViewModel>(name: _viewModelName)
+          .number, _number);
+
+      Registrar.get<MyNotifier>().incrementNumber();
+      await tester.pump();
+
+      expect(find.text('${_number + 1}'), findsOneWidget);
+    });
+  });
+
+  group('MyStatelessViewWidget', () {
+    testWidgets('non-listening stateless View does not update', (WidgetTester tester) async {
+      await tester.pumpWidget(statelessTestApp(listen: false));
+
+      expect(Registrar.isRegistered<MyNotifier>(), true);
+      expect(find.text('$_number'), findsOneWidget);
+
+      Registrar.get<MyNotifier>().incrementNumber();
+      await tester.pump();
+
+      // expect number did not increment (because not listening)
+      expect(find.text('$_number'), findsOneWidget);
+    });
+
+    testWidgets('listening stateless View updates', (WidgetTester tester) async {
+      await tester.pumpWidget(statelessTestApp(listen: true));
+
+      expect(Registrar.isRegistered<MyNotifier>(), true);
+      expect(find.text('$_number'), findsOneWidget);
+
+      Registrar.get<MyNotifier>().incrementNumber();
+      await tester.pump();
+
+      expect(find.text('${_number + 1}'), findsOneWidget);
     });
   });
 }
