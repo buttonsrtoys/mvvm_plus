f6c74b65f8e3d7f4dcc0d0746dea389dbe27457c
buttonsrtoys <buttonsrtoys@gmail.com>
2023-01-28 11:58:36 -0400

    finished tests!

--- example/test/widget_test.dart
+++ example/test/widget_test.dart
@@ -9,7 +9,14 @@ void main() {
     expect(find.text('0'), findsNWidgets(10));
     expect(find.byType(Fab), findsNWidgets(10));
 
-    // await tester.tap(find.byType(Fab));
-    // await tester.pump();
+    final fabs = find.byType(Fab).evaluate().toList();
+    for (final fab in fabs) {
+      await tester.tap(find.byWidget(fab.widget));
+    }
+    await tester.pump(const Duration(seconds: 10)); // Wait for Future and Stream delays
+
+    expect(find.text('1'), findsNWidgets(8));
+    expect(find.text('2'), findsNWidgets(1));
+    expect(find.text('5'), findsNWidgets(1));
   });
 }
